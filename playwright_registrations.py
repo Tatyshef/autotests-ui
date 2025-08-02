ec1fee5fe8cb13aa9a96cbfeb5688bbf65ff8b2b
Tatyana Shevchenko <93898987+Tatyshef@users.noreply.github.com>
2025-08-02 18:37:03 +0300

    работа с авторизацией в Playwright

--- playwright_registrations.py
+++ playwright_registrations.py
@@ -2,8 +2,8 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+    context = browser.new_context() #создаем новый контекст браузера (новая сессия, которая изолирована от других)
+    page = context.new_page() #открываем новую страницу в рамках контекста
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
@@ -19,13 +19,13 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    context.storage_state(path='browser-state.json')
+    context.storage_state(path='browser-state.json') #сохраняем состояние браузера (куки и local storage для дальнейшего использования
 
     page.wait_for_timeout(5000)
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
+    context = browser.new_context(storage_state='browser-state.json') #указываем файл с сохраненным состоянием
     page = context.new_page()
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
