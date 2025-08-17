3a06e9f2913885e0c0255b41f4e6a66df8bab8d9
Tatyana Shevchenko <93898987+Tatyshef@users.noreply.github.com>
2025-08-17 09:56:40 +0300

    Параметризация Pytest

--- tests/test_parametrization.py
+++ tests/test_parametrization.py
@@ -61,3 +61,14 @@ users = {
 )
 def test_identifiers(phone_number: str):
     pass
+
+@pytest.mark.parametrize(
+    "input_value",
+    [
+        pytest.param(1, marks=pytest.mark.xfail(reason="Known issue with 1")),
+        2,
+        pytest.param(3, marks=pytest.mark.skip(reason="Feature not implemented for 3")),
+    ]
+)
+def test_function(input_value):
+    assert input_value != 1
